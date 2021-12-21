f89c5c44fbb5fb44c636672907de52117d486149
Vinicius <viniciuslimaveiga11@gmail.com>
2021-12-21 04:48:48 -0300

    feat: populate dropdown with data from API

--- app/src/main/java/com/app/shoplist/ui/MainActivity.kt
+++ app/src/main/java/com/app/shoplist/ui/MainActivity.kt
@@ -3,12 +3,16 @@ package com.app.shoplist.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import androidx.core.view.isVisible
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.app.shoplist.R
+import com.app.shoplist.data.model.ProductDetails
 import com.app.shoplist.data.network.response.RetrofitInstance
 import com.app.shoplist.databinding.ActivityMainBinding
 import retrofit2.HttpException
+import retrofit2.Response
 import java.io.IOException
 
 const val TAG = "MainActivity"
@@ -17,13 +21,16 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var productAdapter: ProductAdapter
+    private var productsItems: ArrayList<ProductDetails> = ArrayList()
+    private var stateItems: ArrayList<ProductDetails> = ArrayList()
+    private var cityItems: ArrayList<ProductDetails> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setUpRecyclerView()
-        dropdownButton()
+        filterButtonVisibilty()
 
         lifecycleScope.launchWhenCreated {
             binding.progressCircular.isVisible = true
@@ -41,6 +48,9 @@ class MainActivity : AppCompatActivity() {
             }
             if (response.isSuccessful && response.body() != null) {
                 productAdapter.products = response.body()!!
+
+                populateMenuDropdown(response)
+
             } else {
                 Log.e(TAG, "Error: no response")
             }
@@ -55,7 +65,7 @@ class MainActivity : AppCompatActivity() {
             LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
     }
 
-    private fun dropdownButton() {
+    private fun filterButtonVisibilty() {
         binding.ivFilter.setOnClickListener {
             val isMenuVisible = binding.ivDropdownfilters.isVisible
             binding.ivDropdownfilters.isVisible = !isMenuVisible
@@ -65,5 +75,44 @@ class MainActivity : AppCompatActivity() {
             binding.menu2.isVisible = !isMenuVisible
             binding.menu3.isVisible = !isMenuVisible
         }
+    }
+
+    private fun populateMenuDropdown(response: Response<List<ProductDetails>>) {
+
+        //Products
+        productsItems = response.body() as ArrayList<ProductDetails>
+        val data: MutableList<String> = ArrayList()
+        productsItems.forEach { data.add(0, it.brand_name) }
+
+        val adapter = ArrayAdapter<String>(
+            this@MainActivity,
+            R.layout.support_simple_spinner_dropdown_item,
+            data
+        )
+        binding.productsDropdown.setAdapter(adapter)
+
+        //States
+        stateItems = response.body() as ArrayList<ProductDetails>
+        val data2: MutableList<String> = ArrayList()
+        stateItems.forEach { data2.add(0, it.address.state) }
+
+        val adapter2 = ArrayAdapter<String>(
+            this@MainActivity,
+            R.layout.support_simple_spinner_dropdown_item,
+            data2
+        )
+        binding.stateDropdown.setAdapter(adapter2)
+
+        //Cities
+        cityItems = response.body() as ArrayList<ProductDetails>
+        val data3: MutableList<String> = ArrayList()
+        cityItems.forEach { data3.add(0, it.address.city) }
+
+        val adapter3 = ArrayAdapter<String>(
+            this@MainActivity,
+            R.layout.support_simple_spinner_dropdown_item,
+            data3
+        )
+        binding.cityDropdown.setAdapter(adapter3)
     }
 }
